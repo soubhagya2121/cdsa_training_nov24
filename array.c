862c0c86db272446423e7d67b02b050ec33a9e73
soubhagya2121 <soubhagyashindihatti@gmail.com>
2024-11-12 02:32:17 -0800

    day 5 uploded

--- array.c
+++ array.c
@@ -38,3 +38,29 @@ int main()
 
     return 0;
 }
+-------------------------------------------------------------------
+
+include <stdio.h>
+#include <string.h>
+
+int main(int argcount, char *args[]) {
+    if (argc < 3) {
+        printf("Usage: %s\n", args[0]);
+        return 1;
+    }
+
+    char search_char = args[1][0];
+
+    printf("Searching character '%s' in each provided string:\n", search_char);
+
+    for (int i = 2; i < argcount; i++) {
+        char *result = strchr(args[i], search_char);
+
+        if (result != '\0') {
+            printf("character %s to be found is the strin %s is found at position %ld\n", search_char, args[i], result - args[i]);
+        } else {
+            printf("character %s is not found in the string %s\n", search_char, argv[i]);
+        }
+    }
+    return 0;
+}       
